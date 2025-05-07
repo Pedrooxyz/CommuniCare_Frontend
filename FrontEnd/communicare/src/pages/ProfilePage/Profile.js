7cb54120cd75f7f0ba7029f7ede5cf960e68f3e9
Ricardo Fernandes <a27961@alunos.ipca.pt>
2025-05-07 17:10:46 +0100

    Atualização da pagina de perfil

--- FrontEnd/communicare/src/pages/ProfilePage/Profile.js
+++ FrontEnd/communicare/src/pages/ProfilePage/Profile.js
@@ -1,53 +1,56 @@
 import "./Profile.css";
-//import { useState, useEffect } from "react";
-
+import { useNavigate } from "react-router-dom";
 import { FaPlus, FaChevronRight } from 'react-icons/fa'; 
 
+import msgNot from '../../assets/messageNotification.png';
+import notification from '../../assets/notification.png';
 
-import msgNot from '../../assets/messageNotification.png'
-import notification from '../../assets/notification.png'
+import person1 from '../../assets/person1.jpg';
+import plusP from '../../assets/plusProfile.png';
 
-import person1 from '../../assets/person1.jpg'
-import plusP from '../../assets/plusProfile.png'
-
-import cares from '../../assets/Cares.png'
-import loja from '../../assets/loja.png'
-
+import cares from '../../assets/Cares.png';
+import loja from '../../assets/loja.png';
 
 const HeaderNot = () => {
-  return(
+  return (
     <header className="headerNot">
-     <img className="imgHeader" src={msgNot} width={45} height={45} alt="msgNot" />
-     <img className="imgHeader" src={notification} width={45} height={45} alt="Notifications" />
+      <img className="imgHeader" src={msgNot} width={45} height={45} alt="msgNot" />
+      <img className="imgHeader" src={notification} width={45} height={45} alt="Notifications" />
     </header>
-  )
-  
-}
+  );
+};
 
-const DadosUserPI = () =>{
-  return(
+const DadosUserPI = () => {
+  const navigate = useNavigate();
+
+  return (
     <div>
-
       <div className="info">
-
-      <div className="IconProfile">
-        <img className="icPerson" src={person1} width={190} height={190} alt="icProfile" />
-        <img className="iconplus" src={plusP} width={45} height={45} alt="iconplus" />
-      </div>
-
-      <div className="cares-box">
-          <span>
-          <img className="cares" src={cares} width={50} height={50} alt="icProfile" />
-            _ _ _ 100 cares
-          </span>
-          <span>
-            <strong>Loja:</strong> 
-            <img className="loja" src={loja} width={50} height={50} alt="icProfile" />
-          </span>
+        <div className="IconProfile">
+          <img className="icPerson" src={person1} width={190} height={190} alt="icProfile" />
+          <img className="iconplus" src={plusP} width={45} height={45} alt="iconplus" />
         </div>
-     
 
-     </div>
+        <div className="cares-box">
+  <span>
+    <img className="cares" src={cares} width={50} height={50} alt="cares" />
+    _ _ _ 100 cares
+  </span>
+  <span>
+    <strong>Loja:</strong> 
+    <img
+      className="loja"
+      src={loja}
+      width={50}
+      height={50}
+      alt="loja"
+      style={{ cursor: 'pointer' }}
+      onClick={() => navigate('/Loja')}
+    />
+  </span>
+</div>
+
+      </div>
 
       <div className="gridProfile">
         {/* Coluna de Perfil */}
@@ -63,64 +66,55 @@ const DadosUserPI = () =>{
         {/* Coluna de Voluntariados */}
         <div className="cardPofile">
           <h2 className="section">Voluntariados</h2>
-          
+
           <div className="botoes">
-            <button className="action-btn">
-                Pedir Voluntariado <FaPlus style={{ marginLeft: '6px' }} />
+            <button className="action-btn" onClick={() => navigate('/PedirVoluntariado')}>
+              Pedir Voluntariado <FaPlus style={{ marginLeft: '6px' }} />
             </button>
 
-            <button className="sub-action">
-                Pedidos <FaChevronRight style={{ marginLeft: '6px' }} />
+            <button className="sub-action" onClick={() => navigate('/outrosPedidos')}>
+              Pedidos <FaChevronRight style={{ marginLeft: '6px' }} />
             </button>
           </div>
 
-            <div className="recent">
+          <div className="recent">
             <h4 className="recent-name">Recentes:</h4>
             <div className="recent-items">
-                <div className="item">Fuga de água</div>
-                <div className="item">Cortar relva</div>
-                <div className="item">Mudar a Lâmpada</div>
-                <div className="item">Mover Movel</div>
+              <div className="item">Fuga de água</div>
+              <div className="item">Cortar relva</div>
+              <div className="item">Mudar a Lâmpada</div>
+              <div className="item">Mover Movel</div>
             </div>
-            </div>
-
+          </div>
         </div>
 
         {/* Coluna de Empréstimos */}
         <div className="cardPofile">
-            <h2 className="section">Empretimos</h2>
+          <h2 className="section">Empréstimos</h2>
 
-            <div className="botoes">
-            <button className="action-btn">
-              Adicionar Empréstimos <FaPlus style={{ marginLeft: '6px' }}/> 
+          <div className="botoes">
+            <button className="action-btn" onClick={() => navigate('/PedirEmprestimo')}>
+              Adicionar Empréstimos <FaPlus style={{ marginLeft: '6px' }} />
             </button>
 
-            <button className="sub-action">
+            <button className="sub-action" onClick={() => navigate('/MeusEmprestimos')}>
               Empréstimos <FaChevronRight style={{ marginLeft: '6px' }} />
             </button>
+          </div>
 
-            </div>
-
-            <div className="recent">
+          <div className="recent">
             <h4 className="recent-name">Recentes:</h4>
             <div className="recent-items">
-                <div className="item">Secador</div>
-                <div className="item">Martelo</div>
-                <div className="item">Corta relva</div>
-                
+              <div className="item">Secador</div>
+              <div className="item">Martelo</div>
+              <div className="item">Corta relva</div>
             </div>
-            </div>
-
-
           </div>
+        </div>
       </div>
-
     </div>
-      
-  )
-}
-
-
+  );
+};
 
 function Profile() {
   return (
@@ -131,6 +125,4 @@ function Profile() {
   );
 }
 
-
-
-export default Profile
+export default Profile;
